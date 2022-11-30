e59bbc38bcf57213a55fdb2c3b907c5646461d56
Julian Taylor <julian.taylor@capgemini.com>
2022-11-30 10:03:53 +0000

    Add services dictionary

---
+++ app/constants/services-dictionary.js
@@ -0,0 +1,16 @@
+const GRASSLAND_SCHEME = "Grassland Scheme";
+const ARIABLE_LAND_SCHEME = "Arable Land Scheme";
+const MOORLAND_SCHEME = "Moorland Scheme";
+
+const SCHEME_DISPLAY_DICTIONARY = {
+  [GRASSLAND_SCHEME]: "Grassland",
+  [ARIABLE_LAND_SCHEME]: "Arable Land",
+  [MOORLAND_SCHEME]: "Moorland",
+};
+
+module.exports = {
+  ARIABLE_LAND_SCHEME,
+  GRASSLAND_SCHEME,
+  MOORLAND_SCHEME,
+  SCHEME_DISPLAY_DICTIONARY,
+};
